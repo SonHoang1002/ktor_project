5b8d8fe7ebd57906aec993c0e129b030611cf20d
SonHoang1002 <hoangtrungson07012001@gmail.com>
2025-04-17 23:07:22 +0700

    Init Ktor project

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,24 @@
+package com.example
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.example.model.Priority
+import com.example.model.Task
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
